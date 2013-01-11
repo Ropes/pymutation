20344caa46b0c36efd6ed9e3c9c414d61e4c3099
Josh Roppo <ropes@sepor.(none)>
2013-01-10 20:35:28 -0800

    Generic genetic algorithm wrapper classes

---
+++ genetic.py
@@ -0,0 +1,38 @@
+
+
+class Chromosome(object):
+    def __init__(self, par0, par1):
+        pass
+
+    def mutate(self, prob):
+        pass
+
+    def cross(self, prob, chrom0, chrom1):
+        pass
+
+class rna(object):
+    def __init__(self, chrom_list=None, gen_chrom=None):
+        #Create a list of random chromosomes if none provided and generation func provided 
+        pass
+
+    def f(self):
+        '''Evaluate fitness of the population'''
+        pass
+
+class Population(object):
+    def __init__(self):
+        pass
+
+    def crossover(self):
+        prob = 5.0
+        pass
+
+    def mutation(self):
+        prob = 5.0
+        pass
+
+    def selection(self):
+        #Pick the best, mate them
+        #Pick top 60%
+
+
