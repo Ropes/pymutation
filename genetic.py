aaf3d9746751073abd808c81271fb052db895710
Josh Roppo <ropes@sepor.(none)>
2013-01-15 08:32:07 -0800

    Chromosome class will now split it's genes based on key-value orientation(deterministically *knock on wood).

--- genetic.py
+++ genetic.py
@@ -1,16 +1,41 @@
+#!/usr/bin/python
 
+class Gene(dict):
+    pass
 
 class Chromosome(object):
-    def __init__(self, par0, par1):
+    def __init__(self, Genes):
+        self.genes = Genes
+
+    def __str__(self):
+        x = self.__repr__() + '\n'
+        for k,v in self.genes.items():
+            x += k.ljust(5) + str(v) + '\n'
+        return x
+
+    def mutate(self, prob, gene2):
         pass
 
-    def mutate(self, prob):
+
+    def cross(self, prob, gene2):
         pass
 
-    def cross(self, prob, chrom0, chrom1):
-        pass
+    def split(self, percentage_point):
+        x = int(len(self.genes) * percentage_point)
+        print x
+        i = 0
+        a = {}
+        b = {}
+        for k,v in self.genes.items():
+           if i >= x:
+               b[k] = v
+           else:
+               a[k] = v
+           i += 1
+        return (a, b)
+        
 
-class rna(object):
+class Lifeform(object):
     def __init__(self, chrom_list=None, gen_chrom=None):
         #Create a list of random chromosomes if none provided and generation func provided 
         pass
@@ -21,7 +46,7 @@ class rna(object):
 
 class Population(object):
     def __init__(self):
-        pass
+        self.entities = []
 
     def crossover(self):
         prob = 5.0
@@ -34,5 +59,14 @@ class Population(object):
     def selection(self):
         #Pick the best, mate them
         #Pick top 60%
+        pass
 
+if __name__ == '__main__':
+    g0 = {'x': 0, 'y': 1, 'z': 2, 'xx': 12, 'yy': 84, 'zz': 34}
+    g1 = {'x': 5, 'y': 4, 'z': 9, 'xx': 43, 'yy': 193, 'zz': 81}
+
+    c = Chromosome(g0)
+    d = Chromosome(g1)
+    print d
+    print c.split(0.4)
 
