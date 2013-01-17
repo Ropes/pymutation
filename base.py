b697ef128166ba2917dbc62e897e33702f475e09
Josh Roppo <joshroppo@gmail.com>
2013-01-16 23:10:11 -0800

    Changed the setup a little bit by moving Genes and Chromosomes into the base module. Adding mutate functionality.

---
+++ base.py
@@ -0,0 +1,51 @@
+
+import random
+
+class Gene(object):
+    def __init__(self, key, trait, traits,):
+        self.key = key
+        self.trait = trait
+        self.possible_traits = traits
+
+    def __str__(self):
+        return '{}::{} {})'.format(self.key, self.trait,
+                                    self.possible_traits)
+    
+    def mutate(self):
+        self.trait = random.choice(list(self.possible_traits))
+
+
+class Chromosome(object):
+    def __init__(self, genes):
+        self.genes = genes
+
+    def __str__(self):
+        x = self.__repr__() + '\n'
+        for k,v in self.genes.items():
+            x += k.ljust(5) + str(v) + '\n'
+        return x
+
+    def mutate(self, prob):
+        for k,v in self.genes.items():
+            if prob <= random.random():
+                v.mutate()
+                print v
+
+
+
+    def cross(self, prob, gene2):
+        pass
+
+    def split(self, percentage_point):
+        x = int(len(self.genes) * percentage_point)
+        print x
+        i = 0
+        a = {}
+        b = {}
+        for k,v in self.genes.items():
+           if i >= x:
+               b[k] = v
+           else:
+               a[k] = v
+           i += 1
+        return (a, b)
--- genetic.py
+++ genetic.py
@@ -1,53 +1,8 @@
 #!/usr/bin/python
-from random import choice
 
-class Gene(object):
-    def __init__(self, key, trait, traits,):
-        self.key = key
-        self.trait = trait
-        self.possible_traits = traits
+from base import *
 
-    def __str__(self):
-        return '{}::{} {})'.format(self.key, self.trait,
-                                    self.possible_traits)
-    
-    def mutate(self):
-        self.trait = choice(list(self.possible_traits))
-
-
-class Chromosome(object):
-    def __init__(self, Genes):
-        self.genes = Genes
-
-    def __str__(self):
-        x = self.__repr__() + '\n'
-        for k,v in self.genes.items():
-            x += k.ljust(5) + str(v) + '\n'
-        return x
-
-    def mutate(self, prob, gene2):
-        pass
-
-
-    def cross(self, prob, gene2):
-        pass
-
-    def split(self, percentage_point):
-        x = int(len(self.genes) * percentage_point)
-        print x
-        i = 0
-        a = {}
-        b = {}
-        for k,v in self.genes.items():
-           if i >= x:
-               b[k] = v
-           else:
-               a[k] = v
-           i += 1
-        return (a, b)
-        
-
-class Lifeform(object):
+class Being(object):
     def __init__(self, chrom_list=None, gen_chrom=None):
         #TODO: Create a list of random chromosomes if none provided and generation func provided 
         pass
@@ -78,13 +33,15 @@ if __name__ == '__main__':
     g1 = {'x': 5, 'y': 4, 'z': 9, 'xx': 43, 'yy': 193, 'zz': 81}
     c = Chromosome(g0)
     d = Chromosome(g1)
-    #print d
-    #print c.split(0.4)
 
-    g = Gene('x', 'active', frozenset(('active', 'inactive', 'dead')))
-    print g
-    print g.possible_traits
-    g.mutate()
-    print g
+    x = Gene('x', 'action', set(['action', 'inaction', 'protection']))
+    y = Gene('y', 'love', set(['love', 'hate', 'kindness',
+                                'spite', 'malice', 'adoration'])
+    z = Gene('z', 'fire', set(['fire', 'water', 'earth', 'wind'])
+
+    genes = {'x': x, 'y': y, 'z': z}
+    c = Chromosome(genes)
+
+
 
 
